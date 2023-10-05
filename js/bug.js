f38c7afcdb1b0924e89c7cd9e4fb1af11866ac21
MicheleSerafini <serafini.michele01@gmail.com>
2023-10-05 17:38:25 +0200

    Added: other features on script

--- js/bug.js
+++ js/bug.js
@@ -1,7 +1,7 @@
 // CALCOLO BIGLIETTO HTML + JS
 
 // -- recupero gli elementi dal DOM --
-//  input
+//  input km
 const kmInputDomElement = document.getElementById('ticket-km');
 // select
 const discountSelDomElement = document.getElementById('discount');
@@ -9,6 +9,14 @@ const discountSelDomElement = document.getElementById('discount');
 const ctaDomElement = document.querySelector('.cta-calc');
 // ticket price area
 const priceDomElement = document.getElementById('ticket-price');
+// ticket name area
+const nameDomElement = document.getElementById('ticket-name');
+//ticket name input 
+const nameInputDomElement = document.getElementById('user-full-name');
+//ticket preview
+const ticketPreviewDomElement = document.querySelector('.preview-section');
+//ticket offer
+const ticketOfferDescDomElement = document.getElementById('ticket-offer');
 
 // -- ascolto il click sul bottone con un EventListener --
 
@@ -28,14 +36,26 @@ ctaDomElement.addEventListener('click', function () {
   if (ageRange == "1") {
     let discount = ticketPrice * 0.2;
     ticketPrice -= discount;
+    ticketOfferDescDomElement.innerHTML = "Offerta Minorenni";
   } else if (ageRange == "3") {
     let discount = ticketPrice * 0.4;
     ticketPrice -= discount;
+    ticketOfferDescDomElement.innerHTML = "Offerta Over65";
+  } else {
+    ticketOfferDescDomElement.innerHTML = "Offerta Standard";
   }
 
   //arrotondo il ticket price a 2 cifre decimali
   ticketPrice = ticketPrice.toFixed(2);
 
+  //faccio apparire tutto il biglietto
+  ticketPreviewDomElement.classList.remove('d-none');
+  ticketPreviewDomElement.classList.add('d-block');
+
   //stampo il risultato con innerHTML nella ticket area
+  
   priceDomElement.innerHTML = ticketPrice + '&euro;';
+
+  //stampo il nome dell'utente sul biglietto
+  nameDomElement.innerHTML = nameInputDomElement.value;
 })
